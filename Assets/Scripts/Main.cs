734c2846c6009ecf3aeb1e53932d29be0eb1fac5
Basil Gren <bas.gren@gmail.com>
2025-11-19 00:54:22 +0100

    Initial commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hello: " + Application.dataPath);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
