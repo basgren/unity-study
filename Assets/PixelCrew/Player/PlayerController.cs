4c6adebfefe1054cd4baf3a56deca079810361b9
Basil Gren <bas.gren@gmail.com>
2025-11-23 03:28:52 +0100

    Setup basic scene, input System and implement basic input reading
    Includes:
    * cut sprites
    * add sample objects to scene
    * set background and make moving clouds

---
+++ Assets/PixelCrew/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace PixelCrew.Player {
+    public class PlayerController : MonoBehaviour {
+        [SerializeField]
+        private float speed = 2f;
+        
+        private InputActions input;
+        private InputActions.PlayerActions actions;
+
+        private void Awake() {
+            input = new InputActions();
+            actions = input.Player;
+        }
+
+        private void OnEnable() {
+            actions.Enable();
+        }
+
+        private void OnDisable() {
+            actions.Disable();
+        }
+
+        private void OnDestroy() {
+            input.Dispose();
+        }
+
+        void Update() {
+            // We won't use InputSystem events, as order of their invocation is not guaranteed, but
+            // in case we want to check button combinations, it's easier to check them manually.
+            // Using events are better for UI controls.
+            Vector2 dir = actions.Move.ReadValue<Vector2>().normalized;
+            
+            transform.position += new Vector3(dir.x, dir.y, 0) * (speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Background/CloudMover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Background {
+    public class CloudMover : MonoBehaviour {
+        [SerializeField] Transform backgroundObject;
+        [SerializeField] [Range(-1f, 1f)] float moveSpeed = 0.001f;
+
+        Renderer cloudRenderer;
+        Bounds cloudBounds;
+
+        private Renderer backgroundRenderer;
+        float leftEdge;
+        float rightEdge;
+
+        private void Awake() {
+            cloudRenderer = GetComponent<SpriteRenderer>();
+
+            if (backgroundObject != null) {
+                backgroundRenderer = backgroundObject.GetComponent<Renderer>();
+            }
+
+            if (backgroundRenderer == null) {
+                Debug.LogError($"{name}: Background object has no Renderer.");
+            }
+        }
+
+        private void Start() {
+            var bgBounds = backgroundRenderer.bounds;
+            leftEdge = bgBounds.min.x;
+            rightEdge = bgBounds.max.x;
+        }
+
+        void Update() {
+            transform.Translate(Vector3.right * (moveSpeed * Time.deltaTime));
+
+            var bounds = cloudRenderer.bounds;
+            var width = cloudBounds.size.x;
+
+            if (moveSpeed > 0f && bounds.min.x > rightEdge) {
+                transform.position = new Vector3(leftEdge - width, transform.position.y, transform.position.z);
+            } else if (moveSpeed < 0f && bounds.max.x < leftEdge) {
+                transform.position = new Vector3(rightEdge + width, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
