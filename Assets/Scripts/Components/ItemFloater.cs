3d54e19d7eeca4a05d2a88928d10d5080308fb92
Basil Gren <bas.gren@gmail.com>
2025-11-26 05:01:40 +0100

    Implement poitons, add Item Floater component

---
+++ Assets/Scripts/Components/ItemFloater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFloater : MonoBehaviour {
+    [SerializeField]
+    private float period = 2.0f;
+    
+    [SerializeField]
+    private float amplitude = 0.2f;
+
+    [Range(-1.0f, 1.0f)]
+    [SerializeField]
+    private float initialPhase;
+
+    [SerializeField]
+    private bool randomInitialPhase;
+
+    private float time;
+    private Vector3 initialPosition;
+    
+    void Start() {
+        initialPosition = transform.position;
+        
+        if (randomInitialPhase) {
+            initialPhase = Random.Range(-1.0f, 1.0f);
+        }
+    }
+
+    void Update() {
+        var y = Mathf.Cos(time * Mathf.PI * 2.0f / period + initialPhase) * amplitude;
+        transform.position = initialPosition + new Vector3(0, y, 0);
+        time += Time.deltaTime;
+    }
+}
