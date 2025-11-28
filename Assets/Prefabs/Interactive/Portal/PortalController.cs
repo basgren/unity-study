74d82e507d9e03492f0474002bce6b622b9aba53
Basil Gren <bas.gren@gmail.com>
2025-11-28 04:07:45 +0100

    Implement portal

---
+++ Assets/Prefabs/Interactive/Portal/PortalController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prefabs.Interactive.Portal {
+    public class PortalController : MonoBehaviour {
+        [SerializeField]
+        private PortalDestController portalDest;
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player") && portalDest != null) {
+                other.transform.position = portalDest.transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/Prefabs/Interactive/Portal/PortalDestController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+// Currently, this controller is empty as it is used as a filter, so portal can't select other objects
+// except this destination. Also here we can draw gizmos to show required space, so player is not
+// teleported into a wall.
+namespace Prefabs.Interactive.Portal {
+    public class PortalDestController : MonoBehaviour {
+    }
+}
