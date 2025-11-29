16b56ecc47cddcc084f300cfa24bc32b9c2a7687
Basil Gren <bas.gren@gmail.com>
2025-11-29 12:06:10 +0100

    Add particle effects to potions

---
+++ Assets/Scripts/Core/Collectables/PowerUpComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Collectables
+{
+    /// <summary>
+    /// Provides additional effects to power ups.
+    /// </summary>
+    public class PowerUpComponent : MonoBehaviour {
+        [SerializeField]
+        private ParticleSystem effect;
+
+        private ParticleSystem instance;
+        
+        void Start() {
+            if (effect != null) {
+                instance = Instantiate(effect, transform.position, Quaternion.identity);
+            }
+        }
+
+        private void OnDestroy() {
+            if (instance != null) {
+                instance.Stop();
+            }
+        }
+    }
+}
