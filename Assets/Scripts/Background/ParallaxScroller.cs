f94977b89436b76461c135275e4765b6ca1a5e61
Basil Gren <bas.gren@gmail.com>
2025-11-30 03:09:04 +0100

    Fix background shaking with Cinemachine

--- Assets/Scripts/Background/ParallaxScroller.cs
+++ Assets/Scripts/Background/ParallaxScroller.cs
@@ -1,45 +1,47 @@
 ﻿using System;
+using Cinemachine;
 using UnityEngine;
 
 namespace Background {
     public class ParallaxScroller : MonoBehaviour {
-        [SerializeField]
+        [SerializeField] 
         private Vector2 parallaxMultiplier = new Vector2(0.3f, 0.3f);
 
-        private Transform cameraTransform;
-        private Vector3 lastCameraPosition;
-        private float startZ;
+        private CinemachineBrain brain;
+        private Transform cam;
 
-        private void Awake() {
-            if (Camera.main == null) {
-                throw new ArgumentException("Camera.main is null");
-            }
+        private Vector3 startPos;
+        private Vector3 camStartPos;
 
-            cameraTransform = Camera.main.transform;
-            lastCameraPosition = cameraTransform.position;
-            startZ = transform.position.z;
+        private void Awake()
+        {
+            brain = Camera.main.GetComponent<CinemachineBrain>();
+            cam = brain.transform;
+
+            startPos = transform.position;
+            camStartPos = cam.position;
+
+            // подписка на событие
+            CinemachineCore.CameraUpdatedEvent.AddListener(OnCameraUpdated);
         }
 
-        private void LateUpdate() {
-            if (cameraTransform == null) {
-                return;
-            }
+        private void OnDestroy()
+        {
+            CinemachineCore.CameraUpdatedEvent.RemoveListener(OnCameraUpdated);
+        }
 
-            Vector3 cameraDelta = cameraTransform.position - lastCameraPosition;
+        private void OnCameraUpdated(CinemachineBrain b)
+        {
+            // камера полностью обновлена → можно делать параллакс
+            Vector3 camOffset = cam.position - camStartPos;
 
-            Vector3 move = new Vector3(
-                cameraDelta.x * parallaxMultiplier.x,
-                cameraDelta.y * parallaxMultiplier.y,
+            Vector3 offset = new Vector3(
+                camOffset.x * parallaxMultiplier.x,
+                camOffset.y * parallaxMultiplier.y,
                 0f
             );
 
-            transform.position += move;
-
-            // Some recommendation to manually assign fixed Z value in case it starts drifting,
-            // but not sure if it's really needed.
-            // transform.position = new Vector3(transform.position.x, transform.position.y, startZ);
-
-            lastCameraPosition = cameraTransform.position;
+            transform.position = startPos + offset;
         }
     }
 }
