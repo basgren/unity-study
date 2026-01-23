0bf35a8c78fce3f31202a4b0aa7cfe033bd60527
Basil Gren <bas.gren@gmail.com>
2026-01-23 08:56:33 +0100

    Implement pearl desctriction and max travel distance for projectiles

--- Assets/Prefabs/Characters/Common/Projectile.cs
+++ Assets/Prefabs/Characters/Common/Projectile.cs
@@ -10,14 +10,25 @@ namespace Prefabs.Characters.Common {
         [SerializeField]
         private bool invertDirection;
 
+        /// <summary>
+        /// Maximum distance the projectile is allowed to travel. After travelling this distance, it will be destroyed.
+        /// The distance is calculated along this trajectory (even for non-linear).
+        /// </summary>
+        [SerializeField]
+        private float maxTravelDistance = 100f;
+
         private Rigidbody2D myRigidbody;
         private float directionScale;
+
+        private Vector2 prevCoord;
+        private float travelledDistance;
         
         private void Start() {
             myRigidbody = GetComponent<Rigidbody2D>();
             
             // this should be initialized in Start, as in Awake lossyScale is not calculated yet.
             directionScale = transform.lossyScale.x > 0 ? 1 : -1;
+            prevCoord = myRigidbody.position;
         }
 
         private void FixedUpdate() {
@@ -25,6 +36,13 @@ namespace Prefabs.Characters.Common {
             var invert = invertDirection ? -1 : 1;
             pos.x += invert * speed * Time.fixedDeltaTime * directionScale;
             myRigidbody.MovePosition(pos);
+            
+            travelledDistance += Vector2.Distance(pos, prevCoord);
+            prevCoord = pos;
+
+            if (travelledDistance > maxTravelDistance) {
+                Destroy(gameObject);
+            }
         }
     }
 }
