7341e442ce774f57f46dc225c9ae2aa9dd9e2fe7
Basil Gren <bas.gren@gmail.com>
2026-01-26 08:54:59 +0100

    Cleanup traps a bit and make debris disappear with time

--- Assets/Core/Components/Effects/DebrisController.cs
+++ Assets/Core/Components/Effects/DebrisController.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Core.Components.Effects {
-    // TODO: [BG]  Make debris disappear with time, but take "Replay Animation" menu item into account
     // TODO: [BG] Maybe we should freeze z rotation and rotate debris randomly by 90 degrees steps?
-    
+
     public class DebrisController : MonoBehaviour {
         [SerializeField]
         private float minSpeed;
@@ -20,6 +20,15 @@ namespace Core.Components.Effects {
         [SerializeField]
         private float maxRotation;
 
+        [SerializeField]
+        private float minLifeTime = 8f;
+
+        [SerializeField]
+        private float maxLifeTime = 13f;
+
+        [SerializeField]
+        private float fadeOutDuration = 1f;
+
         private DebrisData[] debrisData;
 
         private void Start() {
@@ -28,15 +37,68 @@ namespace Core.Components.Effects {
 
             for (var i = 0; i < rigidBodies.Length; i++) {
                 var rb = rigidBodies[i];
+                var sr = rb.gameObject.GetComponent<SpriteRenderer>();
 
                 debrisData[i] = new DebrisData {
                     rigidbody = rb,
+                    spriteRenderer = sr,
                     initialPosition = rb.transform.localPosition,
-                    initialRotation = rb.transform.localRotation
+                    initialRotation = rb.transform.localRotation,
+                    destroyTime = Time.time + Random.Range(minLifeTime, maxLifeTime),
                 };
             }
 
             PlayAnimation();
+        }
+
+        private void FixedUpdate() {
+            if (gameObject == null) {
+                return;
+            }
+            
+            var destroyedCount = 0;
+
+            for (var i = 0; i < debrisData.Length; i++) {
+                var data = debrisData[i];
+                var isDestroyed = data.rigidbody == null;
+
+                if (isDestroyed) {
+                    destroyedCount++;
+                } else {
+                    if (!data.isDestroying && data.destroyTime < Time.time) {
+                        StartCoroutine(StartFadeOut(data, fadeOutDuration));
+
+                        debrisData[i].isDestroying = true; // override struct value
+                    }
+                }
+            }
+
+            if (destroyedCount == debrisData.Length) {
+                Debug.Log("Destroying full object");
+                Destroy(gameObject);
+            }
+        }
+
+        private IEnumerator StartFadeOut(DebrisData debris, float duration) {
+            while (true) {
+                var sr = debris.spriteRenderer;
+
+                if (sr == null) {
+                    yield break;
+                }
+                
+                var progress = Mathf.Clamp01((Time.time - debris.destroyTime) / duration);
+                if (Mathf.Approximately(progress, 1f)) {
+                    Debug.Log("Destroying debris");
+                    Destroy(sr.gameObject);
+                } else {
+                    var color = sr.color;
+                    color.a = 1f - progress;
+                    sr.color = color;
+                }
+                
+                yield return null;
+            }
         }
 
         [ContextMenu("Replay Animation")]
@@ -71,8 +133,11 @@ namespace Core.Components.Effects {
 
         private struct DebrisData {
             public Rigidbody2D rigidbody;
+            public SpriteRenderer spriteRenderer;
             public Vector3 initialPosition;
             public Quaternion initialRotation;
+            public float destroyTime;
+            public bool isDestroying;
         }
     }
 }
