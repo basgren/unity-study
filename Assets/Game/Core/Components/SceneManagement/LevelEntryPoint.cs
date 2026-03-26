ce97e14df36f4e65beb4932e5bf509757d5f2fee
Basil Gren <bas.gren@gmail.com>
2026-03-26 03:55:13 +0100

    Fix music on levels

---
+++ Assets/Game/Core/Components/SceneManagement/LevelEntryPoint.cs
@@ -0,0 +1,25 @@
+using Core.Audio;
+using Game.Core.Services.Bootstrap;
+using UnityEngine;
+
+namespace Game.Core.Components.SceneManagement {
+    public class LevelEntryPoint : MonoBehaviour {
+        [SerializeField]
+        private AudioCue levelMusic;
+
+        private IAudioLoopHandle audioHandle;
+        
+        void Start() {
+            if (levelMusic != null) {
+                audioHandle = G.Audio.PlayLoopAt(levelMusic, transform.position, false);
+            }
+        }
+        
+        private void OnDestroy() {
+            if (audioHandle != null) {
+                audioHandle.Stop();
+                audioHandle = null;
+            }
+        }
+    }
+}
