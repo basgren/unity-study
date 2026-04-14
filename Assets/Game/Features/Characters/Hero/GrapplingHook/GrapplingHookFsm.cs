efbb304782b170b2903a689ba2e6bbf3a971468d
Basil Gren <bas.gren@gmail.com>
2026-04-14 03:27:04 +0200

    Hook: Don't detach on action button release

--- Assets/Game/Features/Characters/Hero/GrapplingHook/GrapplingHookFsm.cs
+++ Assets/Game/Features/Characters/Hero/GrapplingHook/GrapplingHookFsm.cs
@@ -4,16 +4,13 @@ namespace Game.Features.Characters.Hero.GrapplingHook {
     public enum HookState {
         Idle,
         Shooting,
-        Attached,
-        Retracting
+        Attached
     }
 
     public class GrapplingHookFsm : SimpleStateMachine<HookState> {
         public GrapplingHookFsm() : base(HookState.Idle) {
             Permit(HookState.Idle, HookState.Shooting);
-            Permit(HookState.Shooting, HookState.Attached, HookState.Retracting);
-            Permit(HookState.Attached, HookState.Retracting);
-            Permit(HookState.Retracting, HookState.Idle);
+            Permit(HookState.Shooting, HookState.Attached);
             PermitFromAny(HookState.Idle); // forced abort from any state
         }
     }
